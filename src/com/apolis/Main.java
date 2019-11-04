28092ee954458c66c0196a134e5e4f4c3adf7a08
Sungjin Mo <samuelmo78@gmail.com>
2019-11-04 10:16:43 -0600

    added palindrome

--- src/com/apolis/Main.java
+++ src/com/apolis/Main.java
@@ -6,11 +6,41 @@ public class Main {
         //Generate * triangle
         for (int i=0; i<6; i++)
         {
+            for (int k=0; k<6-i; k++)
+            {
+                System.out.print(" ");
+            }
             for (int j=0; j<i*2+1; j++)
             {
                 System.out.print("*");
             }
             System.out.println("");
         }
+
+        String str = "apple";
+        String str1 = "level";
+
+        if(isPalindrome(str)) {
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+
+
+
+     static boolean isPalindrome(String str) {
+        int i=0;
+        int j=str.length()-1;
+
+        while(i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+
+            i++;
+            j--;
+        }
+        return true;
     }
 }
